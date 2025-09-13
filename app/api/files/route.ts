3701f22cca7cbd6600d5f5a4f228718c15775d3e
Vidhan laddha <74712838+Vidhan0206@users.noreply.github.com>
2025-09-13 22:46:11 +0530

    Update route.ts

--- app/api/files/route.ts
+++ app/api/files/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 import { list } from '@vercel/blob'
+export const dynamic = 'force-dynamic' // This is the line to add
 
 export async function GET() {
   try {
